12250fa4ea80aa12299742ff27fe78a5890aa075
hayatcode <Azharhyatt30@gmail.com>
2025-07-13 08:57:23 +0200

    generatec.js

---
+++ uio/generatec.js
@@ -0,0 +1,40 @@
+'use strict';
+var es = require('event-stream');
+var fs = require('fs');
+var parser = require('weather-alerts-parser');
+var request = require('request');
+var through = require('through');
+
+var geojson = require('../lib/index.js');
+
+
+function cleanProperties() {
+  var remove = [
+    'geocode',
+    'parameter',
+    'polygon'
+  ];
+
+  return through(function write(featureCollection) {
+    featureCollection.features.forEach(function(feature) {
+      remove.forEach(function(remove) {
+        delete feature.properties[remove];
+      });
+    });
+
+    this.queue(featureCollection);
+  });
+}
+
+request.get({
+  url: 'http://alerts.weather.gov/cap/us.php?x=1',
+  headers: {
+    'User-Agent': 'lol',
+  }
+})
+  .pipe(parser.stream())
+  .pipe(geojson.stream({'stylize': true}))
+  .pipe(cleanProperties())
+  .pipe(geojson.collect({'sort': true, 'flatten': true}))
+  .pipe(es.stringify())
+  .pipe(process.stdout);
